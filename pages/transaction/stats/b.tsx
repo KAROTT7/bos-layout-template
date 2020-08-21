f292751f34cad244f7c6c50215c0622498455db3
shuzhiqiang <shuzhiqiang@megalinkps.cn>
2020-08-21 16:43:44 +0800

    将index.tsx中的布局代码移动带到_app.tsx中，并完善菜单递归函数

---
+++ pages/transaction/stats/b.tsx
@@ -0,0 +1,9 @@
+import React, { useState } from 'react'
+
+function Template() {
+  return (
+    <div>b</div>
+  )
+}
+
+export default Template
---
+++ pages/transaction/stats/c.tsx
@@ -0,0 +1,9 @@
+import React, { useState } from 'react'
+
+function Template() {
+  return (
+    <div>c</div>
+  )
+}
+
+export default Template
